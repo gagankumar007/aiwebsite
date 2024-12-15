b8bc4feb5f1b4571750eb3d0ed7913e3df9c2a41
gagankumar007 <ipisc0049@pandoconsultants.com>
2024-12-15 20:33:27 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <div className="navbar">
             <div>
-                <a href="#">Home</a>
+                <a href="#home">Home</a>
                 <a href="#about">About</a>
                 <a href="#services">Services</a>
                 <a href="#contact">Contact</a>
