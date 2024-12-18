12ac1ef99d00e77cde7802f887b5cc5c7ac15da2
gagan-kumar-saini <ipisc0049@pandoconsultants.com>
2024-12-18 15:15:02 +0530

    navbar fixed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 
 import './App.css';
 import About from './components/About';
+import Navbar from './components/Navbar';
 
-function Navbar() {
-        return (
-          <header className="flex justify-between items-center px-8 py-4 bg-white shadow-md">
-            <div className="text-2xl font-bold">
-            Pulseinnovas
-            </div>
-            <nav className="flex space-x-6">
-              <a href="#home" className="text-gray-700 hover:underline">
-                Home
-              </a>
-              <a href="#about" className="text-gray-700 hover:underline">
-                About
-              </a>
-              <a href="#services" className="text-gray-700 hover:underline">
-                Services
-              </a>
-              <a href="#contact" className="text-gray-700 hover:underline">
-                Contact
-              </a>
-            </nav>
-          </header>
-        ); 
-      
-}
 
 function HeroSection() {
     return (
